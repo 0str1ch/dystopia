8ea8cd7ce8363ba2aa73fd746eb61e94ea971fb5
0str1ch <get@smithed.xyz>
2019-01-05 22:30:19 -0500

    Docker integration

--- next.config.js
+++ next.config.js
@@ -33,6 +33,17 @@ const nextConfig = {
   },
 };
 
+module.exports = {
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    mySecret: 'secret',
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    API_URL: process.env.API_URL,
+  },
+};
+
 module.exports = withPlugins(
   [
     [
